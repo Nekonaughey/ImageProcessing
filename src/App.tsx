4eb2654ba93180cd2b97b9eadb4dc95ce0fb20f0
Masahiro Kiyota <masahiro.kiyota@dir.co.jp>
2019-04-12 18:31:30 +0900

    [4-7]ノイズ化処理の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { changeContrast, noise } from './Util';
 
 const App = () => {
   const canvas = useRef<HTMLCanvasElement>(null);
@@ -10,20 +11,8 @@ const App = () => {
       image.onload = () => {
         context.drawImage(image, 0, 0);
         const imageData = context.getImageData(0, 0, image.width, image.height);
-        const pixel = imageData.data;
-
-        // 画面全体のコントラストを変更する。
-        const intensity = 0.2;
-        const result = context.createImageData(imageData);
-        for (let i = 40; i < 70; i++) {
-          for (let j = 50; j < 180; j++) {
-            const index = i * image.width * 4 + j * 4;
-            result.data[index + 0] = (pixel[index + 0] - 128) * intensity + 128;
-            result.data[index + 1] = (pixel[index + 1] - 128) * intensity + 128;
-            result.data[index + 2] = (pixel[index + 2] - 128) * intensity + 128;
-            result.data[index + 3] = (pixel[index + 3] - 128) * intensity + 128;
-          }
-        }
+        // const result = changeContrast(context, imageData, 0.2, 50, 40, 130, 30);
+        const result = noise(context, imageData);
         context.putImageData(result, 0, 0);
       };
       image.src = 'sky.png';
---
+++ src/Util.ts
@@ -0,0 +1,59 @@
+export const changeContrast = (
+  context: CanvasRenderingContext2D,
+  imageData: ImageData,
+  intensity: number,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+): ImageData => {
+  const pixel = imageData.data;
+  const result = context.createImageData(imageData);
+
+  for (let i = 0; i < imageData.height; i += 1) {
+    for (let j = 0; j < imageData.width; j += 1) {
+      const index = i * imageData.width * 4 + j * 4;
+
+      if (y <= i && i < y + height && x <= j && j < x + width) {
+        result.data[index + 0] = (pixel[index + 0] - 128) * intensity + 128;
+        result.data[index + 1] = (pixel[index + 1] - 128) * intensity + 128;
+        result.data[index + 2] = (pixel[index + 2] - 128) * intensity + 128;
+        result.data[index + 3] = (pixel[index + 3] - 128) * intensity + 128;
+      } else {
+        result.data[index + 0] = pixel[index + 0];
+        result.data[index + 1] = pixel[index + 1];
+        result.data[index + 2] = pixel[index + 2];
+        result.data[index + 3] = pixel[index + 3];
+      }
+    }
+  }
+
+  return result;
+};
+
+const clamp = (value: number, min: number, max: number) => {
+  return Math.min(Math.max(value, min), max);
+};
+
+export const noise = (context: CanvasRenderingContext2D, imageData: ImageData): ImageData => {
+  const pixel = imageData.data;
+  const result = context.createImageData(imageData);
+
+  for (let x = 0; x < imageData.height; x += 1) {
+    for (let y = 0; y < imageData.width; y += 1) {
+      const index = x * imageData.width * 4 + y * 4;
+
+      const random = Math.floor(Math.random() * 7) - 3;
+      const sourceX = clamp(x + random, 0, imageData.width - 1);
+      const sourceY = clamp(y + random, 0, imageData.height - 1);
+      const sourceIndex = sourceX * imageData.width * 4 + sourceY * 4;
+
+      result.data[index + 0] = pixel[sourceIndex + 0];
+      result.data[index + 1] = pixel[sourceIndex + 1];
+      result.data[index + 2] = pixel[sourceIndex + 2];
+      result.data[index + 3] = pixel[sourceIndex + 3];
+    }
+  }
+
+  return result;
+};
