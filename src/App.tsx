fec891c515ac682ac1fb4d14826ce54157a40a30
Masahiro Kiyota <masahiro.kiyota@dir.co.jp>
2019-04-12 19:26:38 +0900

    [5-6]画像の一致チェック

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,47 @@ const App = () => {
   useEffect(() => {
     const context = canvas.current && canvas.current.getContext('2d');
     if (context) {
-      const image = new Image();
-      image.onload = () => {
-        context.drawImage(image, 0, 0);
-        const imageData = context.getImageData(0, 0, image.width, image.height);
-        const pixel = imageData.data;
+      const templateImage = new Image();
+      templateImage.onload = () => {
+        context.drawImage(templateImage, 0, 0);
+        const templateImageData = context.getImageData(0, 0, templateImage.width, templateImage.height);
+        const templatePixel = templateImageData.data;
 
-        console.log(cuon);
-        const gray = new Uint8ClampedArray(image.width * image.height);
-        cuon.toGray(pixel, gray);
-        cuon.drawGrayImage(gray, 0, 0, context, imageData);
-        // const result = changeContrast(context, imageData, 0.2, 50, 40, 130, 30);
-        // const result = noise(context, imageData);
-        // const result = grayscale(context, imageData);
-        // context.putImageData(result, 0, 0);
+        context.drawImage(templateImage, 0, 0);
+
+        const templateGray = new Uint8ClampedArray(templateImage.width * templateImage.height);
+        cuon.toGray(templatePixel, templateGray);
+
+        const targetImage = new Image();
+        targetImage.onload = () => {
+          context.drawImage(targetImage, 0, 0);
+
+          const targetImageData = context.getImageData(0, 0, targetImage.width, targetImage.height);
+          const targetPixel = targetImageData.data;
+
+          const targetGray = new Uint8ClampedArray(targetImage.width * targetImage.height);
+          cuon.toGray(targetPixel, targetGray);
+
+          let count = 0;
+          for (let templateY = 0; templateY < templateImage.height; templateY += 1) {
+            for (let templateX = 0; templateX < templateImage.width; templateX += 1) {
+              const index = templateX + templateImage.width * templateY;
+
+              if (targetGray[index] === templateGray[index]) count++;
+            }
+          }
+
+          console.log(count);
+          console.log(templateImage.width * templateImage.height);
+          if (count === templateImage.width * templateImage.height) {
+            alert('一致した');
+          } else {
+            alert('不一致');
+          }
+        };
+        targetImage.src = 'template.png';
       };
-      image.src = 'sky.png';
+      templateImage.src = 'template.png';
     }
   }, []);
 
