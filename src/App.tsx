987cbe45dfa62299137bf05e2d60171e14ec6c8c
Masahiro Kiyota <masahiro.kiyota@dir.co.jp>
2019-04-12 14:47:42 +0900

    [3-4]縦10ピクセルを緑に塗りつぶし

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,21 @@ const App = () => {
       const image = new Image();
       image.onload = () => {
         context.drawImage(image, 0, 0);
-
         const imageData = context.getImageData(0, 0, image.width, image.height);
-        const data = imageData.data;
-        console.log(data[0], data[1], data[2], data[3]);
+
+        console.log(image.width, image.height);
+        // 縦に10ピクセルを緑で塗り潰す
+        for (let i = 0; i < 10; i++) {
+          for (let j = 0; j < image.width * 4; j = j + 4) {
+            const index = i * image.width * 4 + j;
+            console.log(index);
+            imageData.data[index + 0] = 0;
+            imageData.data[index + 1] = 255;
+            imageData.data[index + 2] = 0;
+            imageData.data[index + 3] = 255;
+          }
+        }
+        context.putImageData(imageData, 0, 0);
       };
       image.src = 'sky.png';
     }
